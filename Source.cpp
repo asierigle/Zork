ed25bdc9413f955722bcf37907a044776fe627e0
asierigle <asier.iglesias@yahoo.es>
2015-09-29 11:04:08 +0100

    More things

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*struct Map{
+
+};*/
 
 struct Room
 {
 	char* intro = "WELCOME TO MY AWESOME ZORK, MY LITTLE FRIENDS!!\n";
-	char* name = "You are at the Awesomekitchen\n";
+	char* name = "You are at the ";
 	char* description = "You are trapped inside this beautiful red room in which you only can see a\n\
 						lighter...\n";
 	char* north = NULL;
@@ -22,13 +25,25 @@ struct Room
 int main(int argc, char* argv){
 	bool end = false;
 	Room kitchen;
+	Room street;
+	int x = 2, y = 2;
 	int i = 0;
+
+	char* map[5][5] =
+	{ "Street", "Street", "Scary Forest", "Street", "Street",
+	"Street", "Street", "Street", "Street", "Street",
+	"Street", "Street", "Awesomekitchen", "Street", "Street",
+	"Street", "Street", "Street", "Street", "Street",
+	"Street", "Street", "Street", "Street", "Street"
+	};
 	cout << kitchen.intro;
+	cout << kitchen.name << map[x][y] << endl;
 	cout << kitchen.description;
+	cout << "What are you going to do next?\n";
+
 
 
 	while (!end){
-		cout << "What are you going to do next?\n";
 		string order;
 		getline(cin, order);
 
@@ -41,11 +56,19 @@ int main(int argc, char* argv){
 			i++;
 		}
 		else if (order == "info"){
-			cout << kitchen.name << kitchen.description;
+			cout << kitchen.name << map[x][y] << endl << kitchen.description;
 		}
-		else
+		else if (order == "go north"){
+			--x;
+			cout << street.name << map[x][y] << endl;
+		}
+		else if (order == "why?"){
+			cout << "Because I said so!\n";
+		}
+		else{
 			cout << "You can't do that...\n";
 
+		}
 	}
 	getchar();
 
